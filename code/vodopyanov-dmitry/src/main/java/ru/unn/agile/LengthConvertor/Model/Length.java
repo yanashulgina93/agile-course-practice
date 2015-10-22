4c6323d3730b9ee855daf6cd77ebed94ef0752f5
Dmitry Vodopyanov <dmitryvodop@gmail.com>
2015-10-22 16:06:42 +0300

    resolve some checkstyle issues;

--- code/vodopyanov-dmitry/src/main/java/ru/unn/agile/LengthConvertor/Model/Length.java
+++ code/vodopyanov-dmitry/src/main/java/ru/unn/agile/LengthConvertor/Model/Length.java
@@ -1,13 +1,31 @@
 package ru.unn.agile.LengthConvertor.core;
 
-enum LengthUnit {Inch, Foot, Yard, Mile, Meter, KMeter, CMeter}
+enum LengthUnit { Inch, Foot, Yard, Mile, Meter, KMeter, CMeter }
 
 public class Length {
-    public double value;
-    public LengthUnit UnitInput;
+    private double value;
+    private LengthUnit unitInput;
+
+    Length(double val, LengthUnit un) {
+        this.value = val;
+        this.unitInput = un;
+    }
+
+    @Override
+    public int hashCode() {
+        final int shift = 32;
+
+        long temp = Double.doubleToLongBits(value);
+        int result = (int) (temp ^ (temp >>> shift));
+        return result;
+    }
 
     public boolean equals(final Length length) {
         return length.value == value
-                && length.UnitInput == UnitInput;
+                && length.unitInput == unitInput;
     }
+
+    public double getValue() { return value; }
+
+    public LengthUnit getUnit() { return unitInput; }
 }
--- code/vodopyanov-dmitry/src/main/java/ru/unn/agile/LengthConvertor/Model/LengthConvertor.java
+++ code/vodopyanov-dmitry/src/main/java/ru/unn/agile/LengthConvertor/Model/LengthConvertor.java
@@ -1,14 +1,13 @@
 package ru.unn.agile.LengthConvertor.core;
 
 public class LengthConvertor {
-    public LengthConvertor() {}
+    public LengthConvertor() { }
 
-    public Length Convert(Length length, LengthUnit UnitOutput) {
-        double multiplierMeter[] = {0.0253999998, 0.3048, 0.914399999,
-                                     1609.34401, 1, 1000, 0.01};
+    public Length convert(final Length length, final LengthUnit unitOutput) {
+        double multiplierMeter[] = { 0.0253999998, 0.3048, 0.914399999, 1609.34401, 1, 1000, 0.01 };
         if (length.value >= 0) {
-            length.value = multiplierMeter[length.UnitInput.ordinal()]/
-                           multiplierMeter[UnitOutput.ordinal()]*length.value;
+            length.value = multiplierMeter[length.unitInput.ordinal()] /
+                           multiplierMeter[unitOutput.ordinal()] * length.value;
         } else {
             length.value = -1;
         }
