aee78b1131a85886944b0940f6d73d4973dfd383
Dmitry Vodopyanov <dmitryvodop@gmail.com>
2015-10-22 22:39:37 +0300

    some changes;

--- code/vodopyanov-dmitry/src/main/java/ru/unn/agile/LengthConvertor/Model/LengthConvertor.java
+++ code/vodopyanov-dmitry/src/main/java/ru/unn/agile/LengthConvertor/Model/LengthConvertor.java
@@ -5,8 +5,8 @@ public class LengthConvertor {
 
     public Length convert(final Length length, final LengthUnit unitOutput) {
         double multiplierMeter[] = { 0.0253999998, 0.3048, 0.914399999, 1609.34401, 1, 1000, 0.01 };
-        if (length.value >= 0) {
-            length.value = multiplierMeter[length.unitInput.ordinal()] /
+        if (length.getValue() >= 0) {
+            length.value = multiplierMeter[length.getUnit().ordinal()] /
                            multiplierMeter[unitOutput.ordinal()] * length.value;
         } else {
             length.value = -1;
