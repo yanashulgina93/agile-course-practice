6948c7b13dfbf91400b75bfc5ccf9d7287dabe08
Dmitry Vodopyanov <dmitryvodop@gmail.com>
2015-10-23 01:08:48 +0300

    returned to right constants names (some checkstyle' bug was here) & added a comment to the empty default constructor;

--- code/vodopyanov-dmitry/src/main/java/ru/unn/agile/LengthConvertor/Model/LengthConvertor.java
+++ code/vodopyanov-dmitry/src/main/java/ru/unn/agile/LengthConvertor/Model/LengthConvertor.java
@@ -1,18 +1,20 @@
 package ru.unn.agile.LengthConvertor.Model;
 
 public class LengthConvertor {
-    static final double inch = 0.0253999998;
-    static final double foot = 0.3048;
-    static final double yard = 0.914399999;
-    static final double mile = 1609.34401;
-    static final double meter = 1;
-    static final double kmeter = 1000;
-    static final double cmeter = 0.01;
+    static final double INCH = 0.0253999998;
+    static final double FOOT = 0.3048;
+    static final double YARD = 0.914399999;
+    static final double MILE = 1609.34401;
+    static final double METER = 1;
+    static final double KMETER = 1000;
+    static final double CMETER = 0.01;
 
-    public LengthConvertor() { }
+    public LengthConvertor() {
+        // The default constructor
+    }
 
     public Length convert(final Length length, final LengthUnit unitOutput) {
-        double[] multiplierMeter = {inch, foot, yard, mile, meter, kmeter, cmeter};
+        double[] multiplierMeter = {INCH, FOOT, YARD, MILE, METER, KMETER, CMETER};
         if (length.getValue() >= 0) {
             length.setValue(multiplierMeter[length.getUnit().ordinal()]
                           / multiplierMeter[unitOutput.ordinal()] * length.getValue());
