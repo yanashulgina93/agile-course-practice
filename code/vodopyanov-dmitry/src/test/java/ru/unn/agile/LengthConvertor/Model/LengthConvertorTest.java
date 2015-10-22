e1319ace037510e2a7e4b923d93c48d9b2742e70
Dmitry Vodopyanov <dmitryvodop@gmail.com>
2015-10-23 00:46:37 +0300

    renamed some variables in favour of checkstyle;

--- code/vodopyanov-dmitry/src/test/java/ru/unn/agile/LengthConvertor/Model/LengthConvertorTest.java
+++ code/vodopyanov-dmitry/src/test/java/ru/unn/agile/LengthConvertor/Model/LengthConvertorTest.java
@@ -16,37 +16,37 @@ public class LengthConvertorTest {
     public void areEqualResultsEqual() {
         LengthConvertor z1 = new LengthConvertor();
         Length l1 = new Length(1, LengthUnit.Meter);
-        Length res_z1 = z1.convert(l1, LengthUnit.Yard);
-        assertTrue(res_z1.equals(new Length(1, LengthUnit.Meter)));
+        Length resz1 = z1.convert(l1, LengthUnit.Yard);
+        assertTrue(resz1.equals(new Length(1, LengthUnit.Meter)));
     }
 
     @Test
     public void areResultsWithDifferentValuesNotEqual() {
         LengthConvertor z1 = new LengthConvertor();
         Length l1 = new Length(1, LengthUnit.Meter);
-        Length res_z1 = z1.convert(l1, LengthUnit.Yard);
+        Length resz1 = z1.convert(l1, LengthUnit.Yard);
         LengthConvertor z2 = new LengthConvertor();
         Length l2 = new Length(2, LengthUnit.Meter);
-        Length res_z2 = z2.convert(l2, LengthUnit.Yard);
-        assertFalse(res_z1.equals(res_z2));
+        Length resz2 = z2.convert(l2, LengthUnit.Yard);
+        assertFalse(resz1.equals(resz2));
     }
 
     @Test
     public void areResultsWithDifferentOutputUnitNotEqual() {
         LengthConvertor z1 = new LengthConvertor();
         Length l1 = new Length(1, LengthUnit.Meter);
-        Length res_z1 = z1.convert(l1, LengthUnit.Yard);
+        Length resz1 = z1.convert(l1, LengthUnit.Yard);
         LengthConvertor z2 = new LengthConvertor();
         Length l2 = new Length(1, LengthUnit.Foot);
-        Length res_z2 = z2.convert(l2, LengthUnit.Yard);
-        assertFalse(res_z1.equals(res_z2));
+        Length resz2 = z2.convert(l2, LengthUnit.Yard);
+        assertFalse(resz1.equals(resz2));
     }
 
     @Test
     public void canConvertKMetersToMeters() {
         LengthConvertor z = new LengthConvertor();
         Length l = new Length(1, LengthUnit.KMeter);
-        Length res_z = z.convert(l, LengthUnit.Meter);
-        assertEquals(1000.0, res_z.getValue(), delta);
+        Length resz = z.convert(l, LengthUnit.Meter);
+        assertEquals(1000.0, resz.getValue(), delta);
     }
 }
