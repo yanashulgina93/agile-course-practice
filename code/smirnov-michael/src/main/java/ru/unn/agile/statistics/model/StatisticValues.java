1f3d4eec19fc7eed361a44aa564a7d60a54c8e0d
Michael Smirnov <michael_smirnov@me.com>
2015-10-24 21:58:00 +0300

    Added some another tests on enumeration

--- code/smirnov-michael/src/main/java/ru/unn/agile/statistics/model/StatisticValues.java
+++ code/smirnov-michael/src/main/java/ru/unn/agile/statistics/model/StatisticValues.java
@@ -2,7 +2,35 @@ package ru.unn.agile.statistics.model;
 
 public class StatisticValues
 {
-    public double enumeration(double number) {
-        return 1.0;
+    public StatisticValues(Integer number) {
+        processedData = new double[1];
+        processedData[0] = number;
     }
+
+    public StatisticValues(Integer[] dataForStatistics) {
+        processedData = new double[dataForStatistics.length];
+        for(int i = 0; i < dataForStatistics.length; i++){
+            processedData[i] = dataForStatistics[i];
+        }
+    }
+
+    public StatisticValues(int[] dataForStatistics) {
+        processedData = new double[dataForStatistics.length];
+        for(int i = 0; i < dataForStatistics.length; i++){
+            processedData[i] = dataForStatistics[i];
+        }
+    }
+
+    public double enumeration() {
+        double result = 0.0;
+        for(double instance : processedData)
+        {
+            result += instance;
+        }
+        result /= processedData.length;
+
+        return result;
+    }
+
+    private double[] processedData;
 }
--- code/smirnov-michael/src/test/java/ru/unn/agile/statistics/model/StatisticValuesTest.java
+++ code/smirnov-michael/src/test/java/ru/unn/agile/statistics/model/StatisticValuesTest.java
@@ -1,17 +1,43 @@
 package ru.unn.agile.statistics.model;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class StatisticValuesTest {
     @Test
-    public void isEnumerationOfOneEqualsToOne()
-    {
-        double delta = 1e-8;
-        double number = 1.0;
-        StatisticValues statVal = new StatisticValues();
-        double enumOfNumber = statVal.enumeration(number);
+    public void EnumerationOfOneIsEqualsToOne() {
+        int number = 1;
+        StatisticValues statVal = new StatisticValues(number);
 
-        assertEquals(1.0, enumOfNumber, delta);
+        double enumOfNumber = statVal.enumeration();
+
+        assertEquals(1.0, enumOfNumber, deltaForDoubleAssertEquals);
     }
+
+    @Test
+    public void enumerationOfSimpleIntArrayIsCorrect(){
+        Integer[] data = {5, 5, 5, 5};
+        StatisticValues statVal = new StatisticValues(data);
+
+        double enumOfData = statVal.enumeration();
+
+        assertEquals(5.0, enumOfData, deltaForDoubleAssertEquals);
+    }
+
+    @Test
+    public void enumerationOfIntegerArrayWithDifferentElements()
+    {
+        int[] data = new int[100];
+        for(int i = 0; i < 100; i++){
+            data[i] = i;
+        }
+        StatisticValues statVal = new StatisticValues(data);
+
+        double enumOfData = statVal.enumeration();
+
+        assertEquals(49.5, enumOfData, deltaForDoubleAssertEquals);
+    }
+
+    private double deltaForDoubleAssertEquals = 1e-8;
 }
