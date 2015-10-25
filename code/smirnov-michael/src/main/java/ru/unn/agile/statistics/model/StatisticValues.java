5b4512ebfd99906735c618f977ea4ded44f32c84
Michael Smirnov <michael_smirnov@me.com>
2015-10-25 18:37:08 +0300

    Added tests on probability calculating

--- code/smirnov-michael/src/main/java/ru/unn/agile/statistics/model/StatisticValues.java
+++ code/smirnov-michael/src/main/java/ru/unn/agile/statistics/model/StatisticValues.java
@@ -29,17 +29,37 @@ public class StatisticValues
     }
 
     public double enumeration() {
-        double result = 0.0;
-
-        if(processedData != null) {
-            for (double instance : processedData) {
-                result += instance;
-            }
-            result /= processedData.length;
+        if(processedData == null) {
+            return ENUMERATION_OF_EMPTY_DATA;
         }
+
+        double result = 0.0;
+        for (double instance : processedData) {
+            result += instance;
+        }
+        result /= processedData.length;
 
         return result;
     }
 
+    public double probabilityOf(IStatisticDataInstance event) {
+        if(processedData == null){
+            return PROBABILITY_OF_EVENT_WITH_EMPTY_DATA;
+        }
+
+        double instanceOfEvent = event.getInstance();
+        double probability = 0.0;
+
+        for(double instanceOfData : processedData){
+            if(instanceOfData == instanceOfEvent){
+                probability++;
+            }
+        }
+        probability /= processedData.length;
+        return probability;
+    }
+
     private double[] processedData;
+    private final double ENUMERATION_OF_EMPTY_DATA = 0.0;
+    private final double PROBABILITY_OF_EVENT_WITH_EMPTY_DATA = 0.0;
 }
--- code/smirnov-michael/src/test/java/ru/unn/agile/statistics/model/WhenCalculateStatisticValues.java
+++ code/smirnov-michael/src/test/java/ru/unn/agile/statistics/model/WhenCalculateStatisticValues.java
@@ -13,6 +13,7 @@ public class WhenCalculateStatisticValues {
     public void Preparing()
     {
         statVal = new StatisticValues();
+        dataInstances = null;
     }
 
     @Test
@@ -72,12 +73,70 @@ public class WhenCalculateStatisticValues {
         assertEquals(dataInstances, null);
     }
 
+    @Test
+    public void probabilityOfSingleEventEqualsToOne(){
+        int[] data = {7};
+        IStatisticDataInstance event = new NumericStatisticDataInstance(data[0]);
+        formDataInstances(data);
+
+        checkProbabilityWith(1, event);
+    }
+
+    @Test
+    public void probabilityOfImpossibleEventEqualsToZero(){
+        float[] data = {1.f,2.f,3.f,4.1f,5.f,6.f,7.f,8.f,9.f,10.f};
+        IStatisticDataInstance event = new NumericStatisticDataInstance(4);
+        formDataInstances(data);
+
+        checkProbabilityWith(0.0, event);
+    }
+
+    @Test
+    public void probabilityOfSomePossibleEventIsCorrect(){
+        double[] data = {-1., 3.14, 5., 7.13, 8.25, 3.14};
+        IStatisticDataInstance event = new NumericStatisticDataInstance(3.14f);
+        formDataInstances(data);
+
+        checkProbabilityWith(1/3, event);
+    }
+
+    @Test
+    public void probabilityWithoutDataIsNull(){
+        double[] data = null;
+        IStatisticDataInstance event = new NumericStatisticDataInstance(0);
+        formDataInstances(data);
+
+        checkProbabilityWith(0.0, event);
+    }
+
     private void checkEnumerationWith(final double destination)
     {
         double enumOfData = statVal.enumeration();
         assertEquals(destination, enumOfData, deltaForDoubleAssertEquals);
     }
 
+    private void checkProbabilityWith(final double destination, final IStatisticDataInstance event)
+    {
+        double p = statVal.probabilityOf(event);
+        assertEquals(destination, p, deltaForDoubleAssertEquals);
+    }
+
+    private void formDataInstances(double[] data){
+        Collection<IStatisticDataInstance> dataInstances = StatisticDataConverter.ConvertFromDoubleArray(data);
+        statVal.getData(dataInstances);
+    }
+
+    private void formDataInstances(float[] data){
+        Collection<IStatisticDataInstance> dataInstances = StatisticDataConverter.ConvertFromFloatArray(data);
+        statVal.getData(dataInstances);
+    }
+
+    private void formDataInstances(int[] data){
+        Collection<IStatisticDataInstance> dataInstances = StatisticDataConverter.ConvertFromIntArray(data);
+        statVal.getData(dataInstances);
+    }
+
     private StatisticValues statVal;
+    private Collection<IStatisticDataInstance> dataInstances;
     private double deltaForDoubleAssertEquals = 1e-8;
 }
