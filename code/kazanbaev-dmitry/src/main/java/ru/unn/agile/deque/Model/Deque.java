1af9f4f5f5fb459e4888e23b30561198ab6a5582
dmitrykazanbaev <dmitry.kazanbaev@gmail.com>
2015-10-27 15:48:48 +0300

    added pushBack and pushFront with tests

--- code/kazanbaev-dmitry/src/main/java/ru/unn/agile/deque/Model/Deque.java
+++ code/kazanbaev-dmitry/src/main/java/ru/unn/agile/deque/Model/Deque.java
@@ -13,4 +13,17 @@ public class Deque<T> {
     public Deque(final List<T> list) {
         this.list = list;
     }
+
+    @Override
+    public String toString() {
+        return list.toString();
+    }
+
+    public void pushFront(final T item) {
+        list.add(0, item);
+    }
+
+    public void pushBack(final T item) {
+        list.add(item);
+    }
 }
--- code/kazanbaev-dmitry/src/test/java/ru/unn/agile/Deque/Model/DequeTest.java
+++ code/kazanbaev-dmitry/src/test/java/ru/unn/agile/Deque/Model/DequeTest.java
@@ -20,8 +20,29 @@ public class DequeTest {
         List<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(9);
+
         Deque<Integer> deque = new Deque<>(list);
 
         assertNotNull(deque);
+    }
+
+    @Test
+    public void doesPushFrontWorkProperly() {
+        Deque<Integer> deque = new Deque<>();
+        deque.pushFront(1);
+        deque.pushFront(9);
+        deque.pushFront(-1);
+
+        assertEquals("[-1, 9, 1]", deque.toString());
+    }
+
+    @Test
+    public void doesPushBackWorkProperly() {
+        Deque<Integer> deque = new Deque<>();
+        deque.pushBack(1);
+        deque.pushBack(9);
+        deque.pushBack(-1);
+
+        assertEquals("[1, 9, -1]", deque.toString());
     }
 }
