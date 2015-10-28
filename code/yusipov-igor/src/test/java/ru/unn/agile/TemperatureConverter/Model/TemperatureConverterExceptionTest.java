3bdcece879788abfd94b755dd59724185548b5be
yusipov-igor <yusipov.igor@gmail.com>
2015-10-28 13:16:29 +0300

    Fixed some issues for Travis

--- code/yusipov-igor/src/test/java/ru/unn/agile/TemperatureConverter/Model/TemperatureConverterExceptionTest.java
+++ code/yusipov-igor/src/test/java/ru/unn/agile/TemperatureConverter/Model/TemperatureConverterExceptionTest.java
@@ -1,12 +1,11 @@
 package ru.unn.agile.TemperatureConverter.core;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
 public class TemperatureConverterExceptionTest {
 
     private TemperatureConverterCreator converterCreator = new TemperatureConverterCreator();
-    TemperatureConverter converter;
+    private TemperatureConverter converter;
 
     @Test (expected = IllegalArgumentException.class)
     public void canConvertCelsiusToFahrenheitLargeNumber() {
