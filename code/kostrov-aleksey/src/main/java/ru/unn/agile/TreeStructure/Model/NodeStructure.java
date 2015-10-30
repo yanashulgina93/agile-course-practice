9bbe2f72e47414c738480d4b8f550f1adcfb501c
Aleksey Kostrov <alexkostrov2012@gmail.com>
2015-10-30 14:09:45 +0300

    Covered tests NodeStructure class

--- code/kostrov-aleksey/src/main/java/ru/unn/agile/TreeStructure/Model/NodeStructure.java
+++ code/kostrov-aleksey/src/main/java/ru/unn/agile/TreeStructure/Model/NodeStructure.java
@@ -1,21 +1,43 @@
 package ru.unn.agile.TreeStructure.Model;
 
 
-public class NodeStructure<T extends Comparable> {
-    private T data;
-    private NodeStructure left, right;
+public class NodeStructure<TypeKey extends Comparable, TypeData> {
+    private TypeData data;
+    private final TypeKey key;
+    private NodeStructure leftChild, rightChild;
 
-    public NodeStructure()
-    {
-        left = null;
-        right = null;
+    public NodeStructure(final TypeKey key, final TypeData data) {
+        leftChild = null;
+        rightChild = null;
+        this.key = key;
+        this.data = data;
     }
 
-    public T getData(){
+    public TypeKey getKey() {
+        return key;
+    }
+
+    public TypeData getData() {
         return this.data;
     }
 
-    public void setData(T data){
+    public void setData(final TypeData data) {
         this.data = data;
+    }
+
+    public void setLeftChild(final NodeStructure node) {
+       leftChild = node;
+    }
+
+    public NodeStructure getLeftChild() {
+        return leftChild;
+    }
+
+    public void setRightChild(final NodeStructure node) {
+        rightChild = node;
+    }
+
+    public NodeStructure getRightChild() {
+        return rightChild;
     }
 }
--- code/kostrov-aleksey/src/test/java/ru/unn/agile/TreeStructure/Model/NodeTest.java
+++ code/kostrov-aleksey/src/test/java/ru/unn/agile/TreeStructure/Model/NodeTest.java
@@ -1,16 +1,85 @@
 package ru.unn.agile.TreeStructure.Model;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class NodeTest {
-    private NodeStructure<Integer> node;
+    private NodeStructure<Integer, java.lang.String> nodeWithDataString, nodeChild;
+    private NodeStructure<Integer, Integer> nodeWithDataNumber;
+
+    @Before
+    public void initializeNode() {
+        nodeWithDataString = new NodeStructure<>(1, "initialString");
+    }
+
+    @Test
+    public void canCreateNode() {
+        assertNotNull(nodeWithDataString);
+    }
+
+    @Test
+    public void canGetKey() {
+        nodeWithDataString = new NodeStructure<>(100, "");
+        assertEquals((Integer) 100, nodeWithDataString.getKey());
+    }
+
+    @Test
+    public void canSetString() {
+        nodeWithDataString = new NodeStructure<>(1, "question");
+        nodeWithDataString.setData("answer");
+        assertEquals("answer", nodeWithDataString.getData());
+    }
 
     @Test
     public void canSetNumber() {
-        node = new NodeStructure<>();
-        node.setData(5);
-        assertEquals((Integer)5,node.getData());
+        nodeWithDataNumber = new NodeStructure<>(1, 10);
+        nodeWithDataNumber.setData(20);
+        assertEquals((Integer) 20, nodeWithDataNumber.getData());
+    }
+
+    @Test
+    public void canGetNumber() {
+        nodeWithDataNumber = new NodeStructure<>(10, 10);
+        assertEquals((Integer) 10, nodeWithDataNumber.getData());
+    }
+
+    @Test
+    public void canGetString() {
+        nodeWithDataString = new NodeStructure<>(10, "qwerty");
+        assertEquals("qwerty", nodeWithDataString.getData());
+    }
+
+    @Test
+    public void canSetLeftChild() {
+        nodeWithDataString = new NodeStructure<>(100, "root");
+        nodeChild = new NodeStructure<>(80, "child");
+        nodeWithDataString.setLeftChild(nodeChild);
+        assertEquals("child", nodeWithDataString.getLeftChild().getData());
+    }
+
+    @Test
+    public void canGetLeftChild() {
+        nodeWithDataString = new NodeStructure<>(100, "root");
+        nodeChild = new NodeStructure<>(80, "child");
+        nodeWithDataString.setLeftChild(nodeChild);
+        assertEquals(nodeChild, nodeWithDataString.getLeftChild());
+    }
+
+    @Test
+    public void canSetRightChild() {
+        nodeWithDataString = new NodeStructure<>(100, "root");
+        nodeChild = new NodeStructure<>(80, "child");
+        nodeWithDataString.setRightChild(nodeChild);
+        assertEquals("child", nodeWithDataString.getRightChild().getData());
+    }
+
+    @Test
+    public void canGetRightChild() {
+        nodeWithDataString = new NodeStructure<>(100, "root");
+        nodeChild = new NodeStructure<>(80, "child");
+        nodeWithDataString.setRightChild(nodeChild);
+        assertEquals(nodeChild, nodeWithDataString.getRightChild());
     }
 }
