306a0f3286a1fe4ad0b29750ef3c277ee7aa7967
AnastasiaGarina <aagarina94@gmail.com>
2015-10-30 22:24:27 +0300

    lab1 completion
    added new methods, corrected style mistakes

--- code/garina-anastasia/src/main/java/ru/unn/agile/arabicroman/numeralConverter.java
+++ code/garina-anastasia/src/main/java/ru/unn/agile/arabicroman/numeralConverter.java
@@ -1,54 +1,103 @@
 package ru.unn.agile.arabicroman;
 
-public class numeralConverter {
+public class NumeralConverter {
 
-    private String primaryRomanSymbols [] = {"I", "V", "X", "L", "C", "D", "M"};
-    private Integer numbersRelatedToRomanSymbols [] = {1, 5, 10, 50, 100, 500, 1000};
-
-    String arabicToRoman(int arabicNumber){
-        String romanNumber = "";
-        if (arabicNumber==0) return "";
-
-        Integer divisionResult = 0;
-        Integer positionInNumbersArrays = primaryRomanSymbols.length -1;
-
-        while (positionInNumbersArrays >= 0) {
-            divisionResult = arabicNumber / numbersRelatedToRomanSymbols[positionInNumbersArrays];
-            romanNumber += simpleRomanNumber(divisionResult, positionInNumbersArrays);
-            arabicNumber -= numbersRelatedToRomanSymbols[positionInNumbersArrays]*divisionResult;
-            positionInNumbersArrays-=2;
-        } 
-         return romanNumber;
+    private final String [] primaryRomanSymbols  = {"I", "V", "X", "L", "C", "D", "M"};
+    private final Integer [] numbersRelatedToRomanSymbols  = {1, 5, 10, 50, 100, 500, 1000};
+     String arabicToRoman(final int arabicNumber) {
+        final int maxValidNumber = 3999;
+        if (arabicNumber  < 0 || arabicNumber > maxValidNumber) {
+            throw new IllegalArgumentException();
+        } else {
+            String romanNumber = "";
+            if (arabicNumber == 0) {
+                return "";
+            }
+            int divisionResult;
+            int tempNumber = arabicNumber;
+            int positionInNumbersArrays = primaryRomanSymbols.length - 1;
+            while (positionInNumbersArrays >= 0) {
+                divisionResult = tempNumber
+                        / numbersRelatedToRomanSymbols[positionInNumbersArrays];
+                romanNumber += simpleRomanNumber(divisionResult, positionInNumbersArrays);
+                tempNumber -= numbersRelatedToRomanSymbols[positionInNumbersArrays]
+                        * divisionResult;
+                positionInNumbersArrays -= 2;
+            }
+            return romanNumber;
+        }
     }
 
-    String simpleRomanNumber(Integer divisionResult, Integer pos)
-    {
-        String simpleRoman = "";
-        switch(divisionResult)
-        {
+    private String simpleRomanNumber(final int divisionResult, final int pos) {
+        final int four = 4;
+        final int five = 5;
+        final int nine = 9;
+        switch (divisionResult) {
             case 0: return "";
-            case 4: return primaryRomanSymbols[pos]+primaryRomanSymbols[pos+1];
-            case 9: return primaryRomanSymbols[pos]+primaryRomanSymbols[pos+2];
+            case four: return primaryRomanSymbols[pos]
+                    + primaryRomanSymbols[pos + 1];
+            case nine: return primaryRomanSymbols[pos]
+                    + primaryRomanSymbols[pos + 2];
+            default:
+                String simpleRoman = "";
+                int tempNumber = divisionResult;
+                if (tempNumber >= five) {
+                    simpleRoman = primaryRomanSymbols[pos + 1];
+                    tempNumber -= five;
+                }
+                while (tempNumber > 0) {
+                    simpleRoman += primaryRomanSymbols[pos];
+                    tempNumber--;
+                }
+                return simpleRoman;
         }
-        
-        if (divisionResult>=5){
-            simpleRoman = primaryRomanSymbols[pos+1]; divisionResult -= 5;
-        }
-
-        while (divisionResult > 0)
-        {
-            simpleRoman += primaryRomanSymbols[pos];
-            divisionResult--;
-        }
-        return simpleRoman;
     }
 
-
-    int romanToArabic(String romanNumber){
+    int romanToArabic(final String romanNumber) {
+        if (romanNumber.isEmpty()) {
+            return 0;
+        }
         int arabicNumber = 0;
-        if (romanNumber.isEmpty()) return 0;
-        return 1;
+        int currentPositionOfSymbolInArray =
+                getPosition(romanNumber.charAt(0));
+        arabicNumber += numbersRelatedToRomanSymbols[currentPositionOfSymbolInArray];
+        for (int stringIndex = 1; stringIndex < romanNumber.length(); stringIndex++) {
+            int positionOfNewSymbolInArray =
+                    getPosition(romanNumber.charAt(stringIndex));
+            if (newPositionSurpassOld(currentPositionOfSymbolInArray,
+                    positionOfNewSymbolInArray)) {
+                arabicNumber +=
+                        numbersRelatedToRomanSymbols[positionOfNewSymbolInArray]
+                        - 2 * numbersRelatedToRomanSymbols[currentPositionOfSymbolInArray];
+            } else {
+                if (positionOfNewSymbolInArray <= currentPositionOfSymbolInArray) {
+                    arabicNumber += numbersRelatedToRomanSymbols[positionOfNewSymbolInArray];
+                } else {
+                    throw new IllegalArgumentException();
+                }
+            }
+            currentPositionOfSymbolInArray = positionOfNewSymbolInArray;
+        }
+        return arabicNumber;
     }
 
+    private int getPosition(final char romanSymbol) {
+        for (int pos = 0; pos < primaryRomanSymbols.length; pos++) {
+            if (primaryRomanSymbols[pos].equals(Character.toString(romanSymbol))) {
+                return pos;
+            }
+        }
+        throw new IllegalArgumentException();
 
+    }
+
+    private boolean newPositionSurpassOld(final int oldPos, final int newPos) {
+        if (newPos == (oldPos + 1) ||  newPos == (oldPos + 2)) {
+            if (oldPos % 2 == 1) {
+                throw new IllegalArgumentException();
+            }
+            return true;
+        }
+        return false;
+    }
 }
--- code/garina-anastasia/src/test/java/ru/unn/agile/arabicroman/numeralConverterTest.java
+++ code/garina-anastasia/src/test/java/ru/unn/agile/arabicroman/numeralConverterTest.java
@@ -3,77 +3,116 @@ package ru.unn.agile.arabicroman;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class numeralConverterTest {
+public class NumeralConverterTest {
 
     @Test
-    public void canConvertZeroToEmptyString(){
-        numeralConverter conv = new numeralConverter();
+    public void canConvertZeroToEmptyString() {
+        NumeralConverter conv = new NumeralConverter();
         String romanNumber = conv.arabicToRoman(0);
         assertEquals("", romanNumber);
     }
 
     @Test
-    public void canConvertEmptyStringToZero(){
-        numeralConverter conv = new numeralConverter();
+    public void canConvertEmptyStringToZero() {
+        NumeralConverter conv = new NumeralConverter();
         assertEquals(0, conv.romanToArabic(""));
     }
 
     @Test
-    public void canConvertOneToI(){
-        numeralConverter conv = new numeralConverter();
+    public void canConvertOneToI() {
+        NumeralConverter conv = new NumeralConverter();
         assertEquals("I", conv.arabicToRoman(1));
     }
 
 
     @Test
-    public void canConvertThirtyToXXX(){
-        numeralConverter conv = new numeralConverter();
+    public void canConvertThirtyToXXX() {
+        NumeralConverter conv = new NumeralConverter();
         assertEquals("XXX", conv.arabicToRoman(30));
     }
 
     @Test
-    public void canConvertTwelveToXII(){
-        numeralConverter conv = new numeralConverter();
+    public void canConvertTwelveToXII() {
+        NumeralConverter conv = new NumeralConverter();
         assertEquals("XII", conv.arabicToRoman(12));
-    }
-
-     @Test
-    public void primarySymbolsArrayCreatesCorrectly(){
-        primarySymbols testSymb = new primarySymbols();
-        assertTrue(testSymb.pairs.get(3).getKey()==50 &&  testSymb.pairs.get(3).getValue() =="L");
-
     }
 
     @Test
     public void canConvertEightToVIII() {
-        numeralConverter conv = new numeralConverter();
+        NumeralConverter conv = new NumeralConverter();
         assertEquals("VIII", conv.arabicToRoman(8));
     }
 
     @Test
     public void canConvertFirtyToL() {
-        numeralConverter conv = new numeralConverter();
+        NumeralConverter conv = new NumeralConverter();
         assertEquals("L", conv.arabicToRoman(50));
     }
 
     @Test
     public void canConvertNinetyToXC() {
-        numeralConverter conv = new numeralConverter();
+        NumeralConverter conv = new NumeralConverter();
         assertEquals("XC", conv.arabicToRoman(90));
     }
 
     @Test
     public void canConvert2014ToMMXIV() {
-        numeralConverter conv = new numeralConverter();
+        NumeralConverter conv = new NumeralConverter();
         assertEquals("MMXIV", conv.arabicToRoman(2014));
     }
 
+    @Test
+    public void canConvertIToOne() {
+        NumeralConverter conv = new NumeralConverter();
+        assertEquals(1, conv.romanToArabic("I"));
+    }
+
+    @Test
+    public void canConvertMTo1000() {
+        NumeralConverter conv = new NumeralConverter();
+        assertEquals(1000, conv.romanToArabic("M"));
+    }
+    @Test
+    public void canConvertCCCXXXIIITo333() {
+        NumeralConverter conv = new NumeralConverter();
+        assertEquals(333, conv.romanToArabic("CCCXXXIII"));
+    }
+
+    @Test
+    public void canConvertIXTo9() {
+        NumeralConverter conv = new NumeralConverter();
+        assertEquals(9, conv.romanToArabic("IX"));
+    }
+
+    @Test
+    public void canConvertCMXIVTo914() {
+        NumeralConverter conv = new NumeralConverter();
+        assertEquals(914, conv.romanToArabic("CMXIV"));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void throwOnIncorrectNumberForArabicToRomanConverter() {
+        NumeralConverter conv = new NumeralConverter();
+        conv.arabicToRoman(5000);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void throwOnInvalidSymbolInRomanNumber() {
+        NumeralConverter conv = new NumeralConverter();
+        conv.romanToArabic("KLN");
+    }
 
 
+    @Test (expected = IllegalArgumentException.class)
+    public void throwOnIncorrectOrderOfSymbolsInRomanNumber() {
+        NumeralConverter conv = new NumeralConverter();
+        conv.romanToArabic("IMC");
+    }
 
-
-
-
-
+    @Test (expected = IllegalArgumentException.class)
+    public void throwOnIncorrectWritingOfRomanNumber() {
+        NumeralConverter conv = new NumeralConverter();
+        conv.romanToArabic("VXVI");
+    }
 
 }
