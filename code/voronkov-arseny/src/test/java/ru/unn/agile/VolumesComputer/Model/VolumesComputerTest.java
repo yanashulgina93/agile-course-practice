4887527734d00c67fee5786d29fc00b19d8e7d30
Voronkov-A <arseny-voronkov@yandex.ru>
2015-11-01 14:54:39 +0300

    added test cuboidGoodParams.

--- code/voronkov-arseny/src/test/java/ru/unn/agile/VolumesComputer/Model/VolumesComputerTest.java
+++ code/voronkov-arseny/src/test/java/ru/unn/agile/VolumesComputer/Model/VolumesComputerTest.java
@@ -7,8 +7,11 @@ import org.junit.Test;
 public class VolumesComputerTest {
 
 	@Test
-	public void test() {
-        fail("THIS IS FAIL!!!");
-    }
+	public void cuboidGoodParams() {
+	    final double w = 5.0, h = 4.0, l = 3.0;
+	    final double goodVolume = 60.0;  // 5 * 4 * 3
+	    final double volume = VolumesComputer.cuboid(w, h, l);
+	    assertEquals(volume, goodVolume, m_epsilon);
+	}
 
 }
