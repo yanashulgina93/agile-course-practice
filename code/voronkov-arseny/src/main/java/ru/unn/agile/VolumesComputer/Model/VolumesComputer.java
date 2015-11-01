e695a1adaaaa3be28da37b3e8c9bfc8ffd4e288b
Voronkov-A <arseny-voronkov@yandex.ru>
2015-11-01 14:43:53 +0300

    Added file base and failure test.

---
+++ code/voronkov-arseny/src/main/java/ru/unn/agile/VolumesComputer/Model/VolumesComputer.java
@@ -0,0 +1,5 @@
+﻿package ru.unn.agile.VolumesComputer.Model;
+
+public class VolumesComputer {
+
+}
---
+++ code/voronkov-arseny/src/test/java/ru/unn/agile/VolumesComputer/Model/VolumesComputerTest.java
@@ -0,0 +1,14 @@
+﻿package ru.unn.agile.VolumesComputer.Model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VolumesComputerTest {
+
+	@Test
+	public void test() {
+        fail("THIS IS FAIL!!!");
+    }
+
+}
