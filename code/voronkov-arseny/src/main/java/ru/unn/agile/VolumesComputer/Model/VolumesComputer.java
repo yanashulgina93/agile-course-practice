f73706571ec2bada8c6a603bd0a7ab015f272f5d
Voronkov-A <arseny-voronkov@yandex.ru>
2015-11-01 14:56:01 +0300

    cuboidGoodParams done

--- code/voronkov-arseny/src/main/java/ru/unn/agile/VolumesComputer/Model/VolumesComputer.java
+++ code/voronkov-arseny/src/main/java/ru/unn/agile/VolumesComputer/Model/VolumesComputer.java
@@ -1,5 +1,7 @@
 ﻿package ru.unn.agile.VolumesComputer.Model;
 
 public class VolumesComputer {
-
+    public static double cuboid(double width, double height, double length) {
+        return width * height * length;
+    }
 }
