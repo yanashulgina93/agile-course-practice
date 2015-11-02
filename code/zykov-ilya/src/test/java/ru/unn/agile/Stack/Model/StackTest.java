035bb8af1310a1b22f957f713effc52c4e9ec297
Ilyazykov <ilya.zykov@icloud.com>
2015-11-02 04:20:37 +0300

    refactoring initial test

--- code/zykov-ilya/src/test/java/ru/unn/agile/Stack/Model/StackTest.java
+++ code/zykov-ilya/src/test/java/ru/unn/agile/Stack/Model/StackTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 
 public class StackTest {
     @Test
-    public void Stack_From_Array() {
+    public void Initiating_Stack_From_1_2_3_Array() {
         int[] inputArray = {1,2,3};
         Stack myStack = new Stack(inputArray);
         assertEquals("1 2 3", myStack.toString());
