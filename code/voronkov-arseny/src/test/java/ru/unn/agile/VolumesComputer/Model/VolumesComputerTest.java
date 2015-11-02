dc1d042bceee557bb5f9dbbfbaf30c739b6ba2fd
Voronkov-A <arseny-voronkov@yandex.ru>
2015-11-02 18:30:32 +0300

    formatting corrected again

--- code/voronkov-arseny/src/test/java/ru/unn/agile/VolumesComputer/Model/VolumesComputerTest.java
+++ code/voronkov-arseny/src/test/java/ru/unn/agile/VolumesComputer/Model/VolumesComputerTest.java
@@ -6,34 +6,34 @@ import org.junit.Test;
 
 public class VolumesComputerTest {
 
-	@Test
-	public void cuboidGoodParams() {
-	    final double w = 5.0, h = 4.0, l = 3.0;
-	    final double goodVolume = 60.0;  // 5 * 4 * 3
-	    final double volume = VolumesComputer.cuboid(w, h, l);
-	    assertEquals(volume, goodVolume, EPSILON);
-	}
+    @Test
+    public void cuboidGoodParams() {
+        final double w = 5.0, h = 4.0, l = 3.0;
+        final double goodVolume = 60.0;  // 5 * 4 * 3
+        final double volume = VolumesComputer.cuboid(w, h, l);
+        assertEquals(volume, goodVolume, EPSILON);
+    }
 
     @Test
-	public void cuboidZeroParams() {
-	    final double w = 5.0, h = 4.0, l = 0.0;
+    public void cuboidZeroParams() {
+        final double w = 5.0, h = 4.0, l = 0.0;
         final double goodVolume = 0.0;  // 5 * 4 * 0
         final double volume = VolumesComputer.cuboid(w, h, l);
         assertEquals(volume, goodVolume, EPSILON);
 	}
 
     @Test (expected = IllegalArgumentException.class)
-	public void cuboidNegativeParams() {
-	    VolumesComputer.cuboid(5.0, 4.0, -1.0);
-	}
+    public void cuboidNegativeParams() {
+        VolumesComputer.cuboid(5.0, 4.0, -1.0);
+    }
 
     @Test
-	public void cuboidLargeParams() {
-	    final double w = Double.MAX_VALUE, h = Double.MAX_VALUE, l = 1.0;
+    public void cuboidLargeParams() {
+        final double w = Double.MAX_VALUE, h = Double.MAX_VALUE, l = 1.0;
         final double goodVolume = Double.POSITIVE_INFINITY;
         final double volume = VolumesComputer.cuboid(w, h, l);
         assertEquals(volume, goodVolume, 0.0);
-	}
+    }
 
 
     @Test
