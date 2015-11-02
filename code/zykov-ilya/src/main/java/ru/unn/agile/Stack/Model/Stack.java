b4376b71204b29e886d4e3444ee4a722fa4b5a11
Ilyazykov <ilya.zykov@icloud.com>
2015-11-02 04:09:48 +0300

    Initial test is working

--- code/zykov-ilya/src/main/java/ru/unn/agile/Stack/Model/Stack.java
+++ code/zykov-ilya/src/main/java/ru/unn/agile/Stack/Model/Stack.java
@@ -4,11 +4,20 @@ package ru.unn.agile.Stack.Model;
  * Created by Ilya on 02.11.2015.
  */
 public class Stack {
-    public Stack(int[] inputArray) {
+    private int[] values;
 
+    public Stack(int[] inputArray) {
+        values = inputArray.clone();
     }
 
     public String toString() {
-        return null;
+        String res = "";
+        for (int i = 0; i < values.length; ++i) {
+            res += values[i];
+            if (i != values.length - 1) {
+                res += " ";
+            }
+        }
+        return res;
     }
 }
