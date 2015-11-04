8dcaf4d749541bbc4c1a9748b1bef463706362b0
elenaPlankina <elena.plankina@gmail.com>
2015-11-04 18:33:46 +0300

    Fixing Travis like usually

--- code/plankina-elena/src/test/java/ru/unn/agile/Triangle/Model/TriangleExceptionsTests.java
+++ code/plankina-elena/src/test/java/ru/unn/agile/Triangle/Model/TriangleExceptionsTests.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class TriangleExceptionsTests {
