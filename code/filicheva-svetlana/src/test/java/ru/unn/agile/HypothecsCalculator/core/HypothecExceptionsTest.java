c2ddc1bb23734bb882a1547561597e6d9757e2f2
SvetlanaFilicheva <filicheva.s@gmail.com>
2015-11-05 19:12:39 +0300

    New Travis comments was fixed.

--- code/filicheva-svetlana/src/test/java/ru/unn/agile/HypothecsCalculator/core/HypothecExceptionsTest.java
+++ code/filicheva-svetlana/src/test/java/ru/unn/agile/HypothecsCalculator/core/HypothecExceptionsTest.java
@@ -5,65 +5,65 @@ import java.util.GregorianCalendar;
 import java.util.Calendar;
 
 public class HypothecExceptionsTest {
-    final double creditSum = 18000.0;
-    final int creditPeriod  = 18;
+    private final double creditSum = 18000.0;
+    private final int creditPeriod  = 18;
 
     @Test (expected = IllegalArgumentException.class)
-    public void trowOnNegativeCreditSum(){
+    public void trowOnNegativeCreditSum() {
         final double negativeCreditSum = -18000.0;
         new Hypothec.Builder(negativeCreditSum, creditPeriod).build();
     }
 
     @Test (expected = IllegalArgumentException.class)
-    public void trowOnNegativeCreditPeriod(){
+    public void trowOnNegativeCreditPeriod() {
         final int negativeCreditPeriod  = -18;
         new Hypothec.Builder(creditSum, negativeCreditPeriod).build();
     }
 
     @Test (expected = IllegalArgumentException.class)
-    public void trowOnZeroCreditPeriod(){
+    public void trowOnZeroCreditPeriod() {
         final int zeroCreditPeriod  = 0;
         new Hypothec.Builder(creditSum, zeroCreditPeriod).build();
     }
 
     @Test (expected = IllegalArgumentException.class)
-    public void trowOnNegativeDownPayment(){
+    public void trowOnNegativeDownPayment() {
         final double negativeDownPayment = -1000.0;
         new Hypothec.Builder(creditSum, creditPeriod).setDownPayment(negativeDownPayment).build();
     }
 
     @Test (expected = IllegalArgumentException.class)
-    public void trowOnTooLargeDownPayment(){
+    public void trowOnTooLargeDownPayment() {
         final double tooLargeDownPayment = 20000.0;
         new Hypothec.Builder(creditSum, creditPeriod).setDownPayment(tooLargeDownPayment).build();
     }
 
     @Test (expected = IllegalArgumentException.class)
-    public void trowOnNegativeInterestRate(){
+    public void trowOnNegativeInterestRate() {
         final double negativeInterestRate = -100.0;
         new Hypothec.Builder(creditSum, creditPeriod).setInterestRate(negativeInterestRate).build();
     }
 
     @Test (expected = IllegalArgumentException.class)
-    public void trowOnNegativeMonthlyFee(){
+    public void trowOnNegativeMonthlyFee() {
         final double negativeMonthlyFee = -100.0;
         new Hypothec.Builder(creditSum, creditPeriod).setMonthlyFee(negativeMonthlyFee).build();
     }
 
     @Test (expected = IllegalArgumentException.class)
-    public void trowOnNegativeFlatFee(){
+    public void trowOnNegativeFlatFee() {
         final double negativeFlatFee = -100.0;
         new Hypothec.Builder(creditSum, creditPeriod).setMonthlyFee(negativeFlatFee).build();
     }
 
     @Test (expected = IllegalArgumentException.class)
-    public void trowOnTooEarlyStartDate(){
+    public void trowOnTooEarlyStartDate() {
         final GregorianCalendar tooEarlyDate = new GregorianCalendar(1980, Calendar.MARCH, 10);
         new Hypothec.Builder(creditSum, creditPeriod).setStartDate(tooEarlyDate).build();
     }
 
     @Test (expected = IllegalArgumentException.class)
-    public void trowOnTooLargeStartDate(){
+    public void trowOnTooLargeStartDate() {
         final GregorianCalendar tooLargeDate = new GregorianCalendar(2150, Calendar.MARCH, 10);
         new Hypothec.Builder(creditSum, creditPeriod).setStartDate(tooLargeDate).build();
     }
