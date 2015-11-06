37b0dcd07a17264549cef11111ed6536b65b1504
Voronkov-A <arseny-voronkov@yandex.ru>
2015-11-06 22:23:22 +0300

    code style corrected

--- code/voronkov-arseny/src/main/java/ru/unn/agile/VolumesComputer/Model/VolumesComputer.java
+++ code/voronkov-arseny/src/main/java/ru/unn/agile/VolumesComputer/Model/VolumesComputer.java
@@ -7,14 +7,14 @@ public final class VolumesComputer {
     public static double cuboid(final double width,
                                 final double height,
                                 final double length) {
-        if(width >= 0.0 && height >= 0.0 && length >= 0.0) {
+        if (width >= 0.0 && height >= 0.0 && length >= 0.0) {
             return width * height * length;
         } else {
             throw new IllegalArgumentException(NEGATIVE_PARAMS_ERROR);
         }
     }
     public static double spheroid(final double a, final double b) {
-        if(a >= 0.0 && b >= 0.0) {
+        if (a >= 0.0 && b >= 0.0) {
             final double four = 4.0;
             final double three = 3.0;
             return four / three * Math.PI * a * b * b;
@@ -25,18 +25,20 @@ public final class VolumesComputer {
     public static double rightCylinder(final double a,
                                        final double b,
                                        final double h) {
-        if(a >= 0.0 && b >= 0.0 && h >= 0.0) {
+        if (a >= 0.0 && b >= 0.0 && h >= 0.0) {
             return Math.PI * a * b * h;
         } else {
             throw new IllegalArgumentException(NEGATIVE_PARAMS_ERROR);
         }
     }
     public static double rightCircularCone(final double r, final double h) {
-        if(r >= 0.0 && h >= 0.0) {
+        if (r >= 0.0 && h >= 0.0) {
             final double three = 3.0;
             return Math.PI / three * r * r * h;
         } else {
             throw new IllegalArgumentException(NEGATIVE_PARAMS_ERROR);
         }
+    }
+    private VolumesComputer() {
     }
 }
