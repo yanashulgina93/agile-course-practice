26a40b22545d7b5af27acaae57d48cd253ed7410
Leon-999 <lionewr@gmail.com>
2015-11-06 07:11:13 +0300

    Writed multiply Matrix, changed tests, added IsEqual for compare

--- code/gatchenko-viktor/src/main/java/ru/unn/agile/LongNumber/Matrix.java
+++ code/gatchenko-viktor/src/main/java/ru/unn/agile/LongNumber/Matrix.java
@@ -2,6 +2,8 @@ package main.java.ru.unn.agile.LongNumber;
 
 public class Matrix {
 	
+	static final Matrix UNDEFINED = null;
+	
 	public LongNumber element[][];
 	
 	private int width;
@@ -10,30 +12,82 @@ public class Matrix {
 	public Matrix() {
 		this.width = 0;
 		this.height = 0;
+		this.element = null;
 	}
 	
-	public Matrix(int width, int height) {
+	public Matrix(int height, int width) {
 		this.width = width;
 		this.height = height;
+		
+		this.element = new LongNumber[height][width];
+		for(int i = 0; i < height; ++i) {
+			for(int j = 0; j < width; ++j) {
+				this.element[i][j] = new LongNumber(0);
+			}
+		}
 	}
 	
 	public Matrix Multiply(Matrix secondMultiplier) {
-		return this;
+		Matrix resultMatrix;
+		
+		if(this.width != secondMultiplier.height) {
+			resultMatrix = Matrix.UNDEFINED;
+		} else {
+			resultMatrix = new Matrix(this.height, secondMultiplier.width);
+			
+			for(int i = 0; i < resultMatrix.height; ++i) {
+				for(int j = 0; j < resultMatrix.width; ++j) {
+					resultMatrix.element[i][j] = CountUpElement(i, j, this, secondMultiplier);
+				}
+			}
+		}
+		
+		return resultMatrix;
 	}
 	
-	public int getWidth() {
+	public boolean IsEqual(Matrix secondMatrix) {
+		boolean result = true;
+		
+		if(this.height != secondMatrix.height || this.width != secondMatrix.width) {
+			result = false;
+		} else {
+			for(int i = 0; i < this.height; ++i) {
+				for(int j = 0; j < this.width; ++j) {
+					if(!this.element[i][j].IsEqual(secondMatrix.element[i][j])) {
+						result = false;
+					}
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	private LongNumber CountUpElement(int i, int j, Matrix firstMultiplier, Matrix secondMultiplier) {
+		LongNumber result = new LongNumber();
+		LongNumber partSum;
+		
+		for(int w = 0; w < firstMultiplier.width; ++w) {
+			partSum = firstMultiplier.element[i][w].Multiply(secondMultiplier.element[w][j]);
+			result = result.Add(partSum);
+		}
+		
+		return result;
+	}
+	
+	public int GetWidth() {
 		return this.width;
 	}
 	
-	public void setWidth(int newWidth) {
+	public void SetWidth(int newWidth) {
 		this.width = newWidth;
 	}
 	
-	public int getHeight() {
+	public int GetHeight() {
 		return this.height;
 	}
 	
-	public void setHeight(int newHeight) {
+	public void SetHeight(int newHeight) {
 		this.height = newHeight;
 	}
 	
--- code/gatchenko-viktor/src/test/java/ru/unn/agile/LongMumber/WhenWorkWithMatrix.java
+++ code/gatchenko-viktor/src/test/java/ru/unn/agile/LongMumber/WhenWorkWithMatrix.java
@@ -2,21 +2,64 @@ package test.java.ru.unn.agile.LongMumber;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+
+import main.java.ru.unn.agile.LongNumber.LongNumber;
 import main.java.ru.unn.agile.LongNumber.Matrix;
 
 public class WhenWorkWithMatrix {
 	
 	@Test
-	public void Multiply_Some_Matrix_And_E_Matrix () {
-		Matrix someMatrix = new Matrix();
-		Matrix eMatrix = new Matrix();
+	public void Multiply_Matrix_2x2_And_Matrix_2x2 () {
+		Matrix firstMatrix = new Matrix(2,2);
+			firstMatrix.element[0][0] = new LongNumber(1);
+			firstMatrix.element[0][1] = new LongNumber(1);
+			firstMatrix.element[1][0] = new LongNumber(2);
+			firstMatrix.element[1][1] = new LongNumber(1);
+		Matrix secondMatrix = new Matrix(2,2);
+			secondMatrix.element[0][0] = new LongNumber(2);
+			secondMatrix.element[0][1] = new LongNumber(2);
+			secondMatrix.element[1][0] = new LongNumber(1);
+			secondMatrix.element[1][1] = new LongNumber(2);
+		Matrix gageMatrix = new Matrix(2,2);
+			gageMatrix.element[0][0] = new LongNumber(3);
+			gageMatrix.element[0][1] = new LongNumber(4);
+			gageMatrix.element[1][0] = new LongNumber(5);
+			gageMatrix.element[1][1] = new LongNumber(6);
+			
+		Matrix resultMatrix = firstMatrix.Multiply(secondMatrix);
+		
+		boolean resultCompare = gageMatrix.IsEqual(resultMatrix);
+		assertEquals(true, resultCompare);
+	}
+	
+	@Test
+	public void Multiply_Some_Matrix_5x3_And_E_Matrix_3x3() {
+		Matrix someMatrix = generateMatrix(5, 3);
+		Matrix eMatrix = new Matrix(3,3);
+		eMatrix.element[0][0] = new LongNumber(1);
+		eMatrix.element[1][1] = new LongNumber(1);
+		eMatrix.element[2][2] = new LongNumber(1);
+		
 		Matrix resultMatrix = someMatrix.Multiply(eMatrix);
 		
-		assertEquals(someMatrix, resultMatrix);
+		boolean resultCompare = someMatrix.IsEqual(resultMatrix);
+		assertEquals(true, resultCompare);
 	}
 	
 	//TODO Multiply @Test
-	//TODO Multiply @Test
+	
+	private Matrix generateMatrix(int height, int width) {
+		Matrix resultMatrix = new Matrix(height, width);
+		
+		for(int i = 0; i < height; ++i) {
+			for(int j = 0; j < width; ++j) {
+				int value = (int)(Math.random()*10000);
+				resultMatrix.element[i][j] = new LongNumber(value);
+			}
+		}
+		
+		return resultMatrix;
+	}
 	
 	//@Test
 	/*public void Get_Width_Some_Matrix () {
