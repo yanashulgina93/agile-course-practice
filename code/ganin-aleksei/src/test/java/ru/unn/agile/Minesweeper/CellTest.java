158cde7caed5660edfcf7d317b7ae7d4284fc283
Aleksei Ganin <ganinaleksei@vyadre.com>
2015-11-08 01:18:25 +0300

    refactor

--- code/ganin-aleksei/src/test/java/ru/unn/agile/Minesweeper/CellTest.java
+++ code/ganin-aleksei/src/test/java/ru/unn/agile/Minesweeper/CellTest.java
@@ -3,7 +3,6 @@ package ru.unn.agile.Minesweeper;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class CellTest {
 
