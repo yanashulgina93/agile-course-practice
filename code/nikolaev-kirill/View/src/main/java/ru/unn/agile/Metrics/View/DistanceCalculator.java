fea745a7ef9812a9f25530708e85af7e61751b0f
K. Nikolaev <nikolaevk.95@gmail.com>
2015-11-12 16:39:32 +0300

    Created view

---
+++ code/nikolaev-kirill/View/src/main/java/ru/unn/agile/Metrics/View/DistanceCalculator.java
@@ -0,0 +1,17 @@
+package ru.unn.agile.Metrics.View;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class DistanceCalculator {
+    @FXML
+    private TextField txt1stVector;
+    @FXML
+    private TextField txt2ndVector;
+    @FXML
+    private ToggleGroup tgMetrics;
+    @FXML
+    private Button btnCalc;
+}
