e1b656cfc01c92e8b3b996650f8919def9f55c46
Aleksey Nesmelov <70508010alex@mail.ru>
2015-11-13 12:26:29 +0300

    add path to viewmodel

--- code/nesmelov-aleksey/View/src/main/java/ru/unn/agile/Complex/view/ComplexCalculator.java
+++ code/nesmelov-aleksey/View/src/main/java/ru/unn/agile/Complex/view/ComplexCalculator.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import ru.unn.agile.Complex.viewmodel.*;
 
 public class ComplexCalculator {
     @FXML
