13fd3b665491d7b77fcb4ba9afa63f8dfc3c11a1
SmirnovVyacheslav <necrolazy@gmail.com>
2015-11-14 03:02:52 +0300

    Fix copy paste error

--- code/smirnov-vyacheslav/src/test/java/ru/unn/agile/Polinom/Model/PolinomMultiplyTest.java
+++ code/smirnov-vyacheslav/src/test/java/ru/unn/agile/Polinom/Model/PolinomMultiplyTest.java
@@ -29,11 +29,11 @@ public class PolinomMultiplyTest {
     @Parameterized.Parameters
     public static List<Object[]> MultiplyInputAndExpected() {
         return Arrays.asList(new Object[][] {
-            {new double[]{0.0}, new double[]{0.0}, new double[]{0.0}},
             {new double[]{7.0, 15.0}, new double[]{0.0}, new double[]{0.0}},
             {new double[]{7.0, 15.0}, new double[]{4.0}, new double[]{28.0, 60.0}},
             {new double[]{7.0, 15.0}, new double[]{0.0, 4.0}, new double[]{0.0, 28.0, 60.0}},
             {new double[]{7.0, 15.0}, new double[]{4.0, 7.0}, new double[]{28.0, 154.0, 105.0}},
+            {new double[]{0.0}, new double[]{0.0}, new double[]{0.0}},
             {new double[]{5.0, 6.0, -7.0}, new double[]{-5.0, 7.0}, new double[]{25.0, -65.0, 77.0, -49.0}},
             {new double[]{2.0, 0.0, -6.0}, new double[]{1.25, 0.5}, new double[]{2.5, 1.0, -7.5, -3.0}}
         });
