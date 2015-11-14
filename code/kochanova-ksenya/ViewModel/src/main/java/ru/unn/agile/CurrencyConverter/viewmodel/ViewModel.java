a99c91aa385ce4bcb8473fb5fdf1e35ae239d2e2
KsenyaKochanova <ksenya.kochanova@gmail.com>
2015-11-14 21:43:13 +0300

    added ViewModel

---
+++ code/kochanova-ksenya/ViewModel/src/main/java/ru/unn/agile/CurrencyConverter/viewmodel/ViewModel.java
@@ -0,0 +1,126 @@
+package ru.unn.agile.CurrencyConverter.viewmodel;
+
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.*;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import ru.unn.agile.CurrencyConverter.Model.Currency;
+import ru.unn.agile.CurrencyConverter.Model.UnitCurrency;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewModel {
+    private final StringProperty inputValue = new SimpleStringProperty();
+    private final StringProperty outputValue = new SimpleStringProperty();
+
+    private final ObjectProperty<UnitCurrency> inputUnit = new SimpleObjectProperty<>();
+    private final ObjectProperty<UnitCurrency> outputUnit = new SimpleObjectProperty<>();
+
+    private final BooleanProperty convertationDisable = new SimpleBooleanProperty();
+
+    private final StringProperty status = new SimpleStringProperty();
+
+    private final List<ValueChangeListener> valueChangedListeners = new ArrayList<>();
+
+    public ViewModel() {
+        inputValue.set("0.00");
+        outputValue.set("0.00");
+        status.set(Status.WAITING.toString());
+        inputUnit.set(UnitCurrency.RUBLE);
+        outputUnit.set(UnitCurrency.RUBLE);
+
+        BooleanBinding couldConvert = new BooleanBinding() {
+            {
+                super.bind(inputValue);
+            }
+            @Override
+            protected boolean computeValue() {
+                return getInputStatus() == Status.READY;
+            }
+        };
+        convertationDisable.bind(couldConvert.not());
+        final List<StringProperty> fields = new ArrayList<StringProperty>() { {
+            add(inputValue);
+        } };
+
+        for (StringProperty field : fields) {
+            final ValueChangeListener listener = new ValueChangeListener();
+            field.addListener(listener);
+            valueChangedListeners.add(listener);
+        }
+    }
+
+    public void convert() {
+        if (convertationDisable.get()) {
+            return;
+        }
+
+        Currency inputCurrency = new Currency(inputValue.getValue(), inputUnit.get());
+        outputValue.set(outputUnit.get().convertCurrency(inputCurrency,
+                outputUnit.get()).getValueInStringFormat());
+        status.set(Status.DONE.toString());
+    }
+
+    public StringProperty inputValueProperty() {
+        return inputValue;
+    }
+    public StringProperty outputValueProperty() {
+        return outputValue;
+    }
+
+    public final ObjectProperty<UnitCurrency> inputUnitProperty() {
+        return inputUnit;
+    }
+    public final ObjectProperty<UnitCurrency> outputUnitProperty() {
+        return outputUnit;
+    }
+
+
+    public BooleanProperty convertationDisableProperty() {
+        return convertationDisable;
+    }
+
+    public StringProperty statusProperty() {
+        return status;
+    }
+    private Status getInputStatus() {
+        Status inputStatus = Status.READY;
+        if (inputValue.get().isEmpty()) {
+            inputStatus = Status.WAITING;
+        }
+        try {
+            if (!inputValue.get().isEmpty()) {
+                Double.parseDouble(inputValue.get());
+            }
+        } catch (NumberFormatException e) {
+            inputStatus = Status.WRONG_FORMAT;
+        }
+
+        return inputStatus;
+    }
+
+    private class ValueChangeListener implements ChangeListener<String> {
+        @Override
+        public void changed(final ObservableValue<? extends String> observable,
+                            final String oldValue, final String newValue) {
+            status.set(getInputStatus().toString());
+        }
+    }
+}
+
+enum Status {
+    WAITING("Please, input value and choose currency units"),
+    READY("Press 'Convert'"),
+    WRONG_FORMAT("Wrong format, please, use 0.00 format"),
+    DONE("Done");
+
+    private final String name;
+    private Status(final String name) {
+        this.name = name;
+    }
+    public String toString() {
+        return name;
+    }
+}
---
+++ code/kochanova-ksenya/ViewModel/src/test/java/ru/unn/agile/CurrencyConverter/viewmodel/ViewModelTests.java
@@ -0,0 +1,116 @@
+package ru.unn.agile.CurrencyConverter.viewmodel;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import ru.unn.agile.CurrencyConverter.Model.UnitCurrency;
+
+import static org.junit.Assert.*;
+
+public class ViewModelTests {
+    private ViewModel viewModel;
+
+    @Before
+    public void setUp() {
+        viewModel = new ViewModel();
+    }
+
+    @After
+    public void tearDown() {
+        viewModel = null;
+    }
+
+    @Test
+    public void canSetDefaultValueAndUnits() {
+        assertEquals("0.00", viewModel.inputValueProperty().get());
+        assertEquals("0.00", viewModel.inputValueProperty().get());
+        assertEquals(UnitCurrency.RUBLE, viewModel.inputUnitProperty().get());
+        assertEquals(UnitCurrency.RUBLE, viewModel.outputUnitProperty().get());
+        assertEquals(Status.WAITING.toString(), viewModel.statusProperty().get());
+    }
+
+    @Test
+    public void statusIsWaitingWhenValueFieldEmpty() {
+        viewModel.inputValueProperty().set("");
+
+        viewModel.convert();
+
+        assertEquals(Status.WAITING.toString(), viewModel.statusProperty().get());
+    }
+
+    @Test
+    public void statusIsReadyWhenInputValueIsCorrect() {
+        viewModel.inputValueProperty().set("500.70");
+
+        assertEquals(Status.READY.toString(), viewModel.statusProperty().get());
+    }
+
+    @Test
+    public void canDetectWrongFormat() {
+        viewModel.inputValueProperty().set("^-^");
+
+        assertEquals(Status.WRONG_FORMAT.toString(), viewModel.statusProperty().get());
+    }
+
+    @Test
+    public void rubleIsDefaultCurrency() {
+
+        assertEquals(UnitCurrency.RUBLE, viewModel.inputUnitProperty().get());
+        assertEquals(UnitCurrency.RUBLE, viewModel.outputUnitProperty().get());
+    }
+
+    @Test
+    public void convertButtonIsDisabledWhenFormatIsWrong() {
+
+        viewModel.inputValueProperty().set("1,1");
+
+        assertTrue(viewModel.convertationDisableProperty().get());
+    }
+
+    @Test
+    public void calculateButtonIsDisabledWithIncompleteInput() {
+        viewModel.inputValueProperty().set(".");
+
+        assertTrue(viewModel.convertationDisableProperty().get());
+    }
+
+    @Test
+    public void canSetDoneMessageWhenConvertationIsDone() {
+        viewModel.inputValueProperty().set("70");
+        viewModel.inputUnitProperty().set(UnitCurrency.RUBLE);
+        viewModel.outputUnitProperty().set(UnitCurrency.DOLLAR);
+
+        viewModel.convert();
+
+        assertEquals(Status.DONE.toString(), viewModel.statusProperty().get());
+    }
+
+    @Test
+    public void canSetWrongFormatMessage() {
+        viewModel.inputValueProperty().set("^-^");
+
+        assertEquals(Status.WRONG_FORMAT.toString(), viewModel.statusProperty().get());
+    }
+
+    @Test
+    public void statusIsReadyWhenDataIsCorrect() {
+        viewModel.inputValueProperty().set("135.20");
+        viewModel.inputUnitProperty().set(UnitCurrency.EURO);
+        viewModel.outputUnitProperty().set(UnitCurrency.DOLLAR);
+
+        assertEquals(Status.READY.toString(), viewModel.statusProperty().get());
+    }
+
+    @Test
+    public void convertRubleToDollarIsCorrect() {
+        viewModel.inputValueProperty().set("100");
+        viewModel.inputUnitProperty().set(UnitCurrency.RUBLE);
+        viewModel.outputUnitProperty().set(UnitCurrency.DOLLAR);
+
+        viewModel.convert();
+
+        assertEquals("1.6", viewModel.outputValueProperty().get());
+        assertEquals(UnitCurrency.DOLLAR, viewModel.outputUnitProperty().get());
+    }
+
+}
