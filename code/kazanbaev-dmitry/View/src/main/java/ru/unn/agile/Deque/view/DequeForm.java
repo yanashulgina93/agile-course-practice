2636c46c40e566e5998df78eaadadbaf13713bfb
dmitrykazanbaev <dmitry.kazanbaev@gmail.com>
2015-11-14 21:00:03 +0300

    refactoring

--- code/kazanbaev-dmitry/View/src/main/java/ru/unn/agile/Deque/view/DequeForm.java
+++ code/kazanbaev-dmitry/View/src/main/java/ru/unn/agile/Deque/view/DequeForm.java
@@ -2,22 +2,20 @@ package ru.unn.agile.Deque.view;
 
 import javax.swing.*;
 
-public class DequeForm {
+public final class DequeForm {
     private JPanel mainPanel;
     private JTextField inputNumberText;
     private JButton pushFrontButton;
     private JButton pushBackButton;
-    private JTable dequeTable;
 
-    public static void main(String[] args) {
+    private DequeForm() {
+    }
+
+    public static void main(final String[] args) {
         JFrame frame = new JFrame("DequeForm");
         frame.setContentPane(new DequeForm().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-    }
-
-    private void createUIComponents() {
-        dequeTable = new JTable(new String[][]{{"1"},{"2"}}, new String[]{"Front"});
     }
 }
