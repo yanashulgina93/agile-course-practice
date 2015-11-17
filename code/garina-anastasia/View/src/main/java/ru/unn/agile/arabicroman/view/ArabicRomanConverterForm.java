370fe118d013e461d3da7cbceb4eee72c9174e3c
AnastasiaGarina <aagarina94@gmail.com>
2015-11-17 18:32:08 +0300

    simple form added

---
+++ code/garina-anastasia/View/src/main/java/ru/unn/agile/arabicroman/view/ArabicRomanConverterForm.java
@@ -0,0 +1,10 @@
+package ru.unn.agile.arabicroman.view;
+
+import javax.swing.*;
+
+public class ArabicRomanConverterForm {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton convertButton;
+    private JButton reverseButton;
+}
