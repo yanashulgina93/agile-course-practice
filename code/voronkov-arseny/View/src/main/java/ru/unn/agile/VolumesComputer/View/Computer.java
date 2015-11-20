f1c012666e99c0a077f2a1392bb376c959f8edeb
Voronkov-A <arseny-voronkov@yandex.ru>
2015-11-21 02:01:08 +0300

    add view form

---
+++ code/voronkov-arseny/View/src/main/java/ru/unn/agile/VolumesComputer/View/Computer.java
@@ -0,0 +1,17 @@
+package ru.unn.agile.VolumesComputer.View;
+
+import javax.swing.*;
+
+public class Computer {
+    private JComboBox comboBoxFigures;
+    private JTextField textFieldIn1;
+    private JTextField textFieldIn2;
+    private JTextField textFieldIn3;
+    private JTextField textFieldOut;
+    private JPanel panelMain;
+    private JLabel labelIn1;
+    private JLabel labelIn2;
+    private JLabel labelIn3;
+    private JLabel labelOut;
+    private JButton buttonSolve;
+}
