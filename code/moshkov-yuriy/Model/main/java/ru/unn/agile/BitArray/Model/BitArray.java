f5286717d2ee844040a07bbc914e301826fa8033
juraam <juramoshkov@gmail.com>
2015-11-20 15:01:01 +0300

    Restructure project

---
+++ code/moshkov-yuriy/Model/main/java/ru/unn/agile/BitArray/Model/BitArray.java
@@ -0,0 +1,93 @@
+package ru.unn.agile.BitArray.core;
+
+import java.util.Arrays;
+
+public class BitArray {
+    private final Boolean[] arrayBit;
+    private final int size;
+
+    public BitArray(final int size) {
+        arrayBit = new Boolean[size];
+        Arrays.fill(arrayBit, false);
+        size = size;
+    }
+
+    public Boolean getBit(final int index) {
+        return arrayBit[index];
+    }
+
+    public void setBit(final int index, final Boolean value) {
+        if (value == null) {
+            return;
+        }
+        arrayBit[index] = value;
+    }
+
+    public void setAll(final Boolean value) {
+        Arrays.fill(arrayBit, value);
+    }
+
+    public BitArray and(final BitArray bitArray) {
+        return doOperationWithArray(bitArray, new AndOperation());
+    }
+
+    public BitArray or(final BitArray bitArray) {
+        return doOperationWithArray(bitArray, new OrOperation());
+    }
+
+    public BitArray xor(final BitArray bitArray) {
+        return doOperationWithArray(bitArray, new XorOperation());
+
+    }
+
+    public BitArray not() {
+        BitArray res = new BitArray(size);
+        for (int i = 0; i < size; i++) {
+            res.setBit(i, !getBit(i));
+        }
+        return res;
+    }
+
+    public Boolean[] getBitArray() {
+        return arrayBit;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    private BitArray doOperationWithArray(final BitArray bitArray, final IOperation operation) {
+        BitArray res;
+        if (bitArray.getSize() == size) {
+            res = new BitArray(size);
+            for (int i = 0; i < size; i++) {
+                res.setBit(i, operation.doOperation(getBit(i), bitArray.getBit(i)));
+            }
+        } else {
+            throw new BitArrayDifferentSizeException("Different size of arrays");
+        }
+        return res;
+    }
+
+    private class XorOperation implements IOperation {
+        @Override
+        public Boolean doOperation(final Boolean first, final Boolean second) {
+            return first ^ second;
+        }
+    }
+
+    private class OrOperation implements IOperation {
+        @Override
+        public Boolean doOperation(final Boolean first, final Boolean second) {
+            return first | second;
+        }
+    }
+
+    private class AndOperation implements IOperation {
+        @Override
+        public Boolean doOperation(final Boolean first, final Boolean second) {
+            return first & second;
+        }
+    }
+}
+
---
+++ code/moshkov-yuriy/View/src/main/java/ru/unn/agile/BitArray/view/BitArrayForm.java
@@ -0,0 +1,14 @@
+package ru.unn.agile.BitArray.view;
+
+import javax.swing.*;
+
+/**
+ * Created by ruymoshkov on 11/20/2015.
+ */
+public class BitArrayForm {
+    private JPanel mainPanel;
+    private JTextField txtSizeArray;
+    private JButton btnInitArray;
+    private JTable table1;
+    private JTable table2;
+}
