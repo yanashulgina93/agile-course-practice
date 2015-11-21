21a31af09640b86a46ed63fedb8adf625233c27c
juramoshkov <juramoshkov@gmail.com>
2015-11-21 19:33:22 +0300

    Add view and view model classes

--- code/moshkov-yuriy/View/src/main/java/ru/unn/agile/BitArray/view/BitArrayForm.java
+++ code/moshkov-yuriy/View/src/main/java/ru/unn/agile/BitArray/view/BitArrayForm.java
@@ -6,9 +6,27 @@ import javax.swing.*;
  * Created by ruymoshkov on 11/20/2015.
  */
 public class BitArrayForm {
+    private ViewModel viewModel;
+
     private JPanel mainPanel;
-    private JTextField txtSizeArray;
-    private JButton btnInitArray;
-    private JTable table1;
-    private JTable table2;
+    private JTextField sizeArrayTxtFld;
+    private JButton initArrayBtn;
+    private JTable bitArrayFirstTable;
+    private JTable bitArraySecondTable;
+
+    private BitArrayForm() { }
+
+    private BitArrayForm(final ViewModel viewModel) {
+        this.viewModel = viewModel;
+//        backBind();
+    }
+
+    public static void main(final String[] args) {
+        JFrame frame = new JFrame("BitArrayForm");
+
+        frame.setContentPane(new Calculator(new ViewModel()).mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
---
+++ code/moshkov-yuriy/ViewModel/src/main/java/ru/unn/agile/BitArray/viewmodel/ViewModel.java
@@ -0,0 +1,9 @@
+package ru.unn.agile.BitArray.viewmodel;
+
+import ru.unn.agile.BitArray.model;
+
+public class ViewModel {
+    public ViewModel() {
+
+    }
+}
