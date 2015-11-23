af0465c86c0ff92f6f0c2efee3b14a63ad1040bf
Aleksei Ganin <ganinaleksei@vyadre.com>
2015-11-24 00:20:50 +0300

    rm unused import

--- code/ganin-aleksei/View/src/test/java/ru/unn/agile/Minesweeper/View/ViewTest.java
+++ code/ganin-aleksei/View/src/test/java/ru/unn/agile/Minesweeper/View/ViewTest.java
@@ -3,7 +3,6 @@ package ru.unn.agile.Minesweeper.View;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class ViewTest {
