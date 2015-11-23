a99596aef647a0eed6d9b93c1510aa35a08871b3
Molotkov Andrei <molotkov.and@gmail.com>
2015-11-23 13:54:49 +0300

    added view-model

--- code/molotkov-andrei/ViewModel/src/main/java/ru/unn/NewtonMethod/viewModel/NewtonMethodViewModel.java
+++ code/molotkov-andrei/ViewModel/src/main/java/ru/unn/NewtonMethod/viewModel/NewtonMethodViewModel.java
@@ -157,8 +157,7 @@ public class NewtonMethodViewModel {
         return !function.isEmpty() && !derivative.isEmpty() && !leftPointOfRange.isEmpty()
                 && !rightPointOfRange.isEmpty();
     }
-
-
+    
     private void calculateRoot() {
         NewtonMethod newtonMethod = new NewtonMethod(function, derivative);
         try {
