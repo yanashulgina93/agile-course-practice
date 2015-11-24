dd387e5cd3754123c6c090b9500a8f9e921cc878
Aleksei Ganin <ganinaleksei@vyadre.com>
2015-11-24 09:38:05 +0300

    View/Main fix Main

--- code/ganin-aleksei/View/src/main/java/ru/unn/agile/Minesweeper/View/Main.java
+++ code/ganin-aleksei/View/src/main/java/ru/unn/agile/Minesweeper/View/Main.java
@@ -1,11 +1,11 @@
 package ru.unn.agile.Minesweeper.View;
 
-final public class Main {
+public class Main {
     public static void main(final String[] args) {
         new View();
     }
 
-    private Main() {
+    Main() {
 
     }
 }
