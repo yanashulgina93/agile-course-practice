1262c1319f3d6610a2da3c3234b1699ef9594d3d
Voronkov-A <arseny-voronkov@yandex.ru>
2015-11-25 16:43:16 +0300

    fix ftf again

--- code/voronkov-arseny/View/src/test/java/ru/unn/agile/VolumesComputer/View/ComputerTest.java
+++ code/voronkov-arseny/View/src/test/java/ru/unn/agile/VolumesComputer/View/ComputerTest.java
@@ -1,16 +1,26 @@
 package ru.unn.agile.VolumesComputer.View;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import ru.unn.agile.VolumesComputer.ViewModel.ComputerViewModel;
 
 import static org.junit.Assert.*;
 
 public class ComputerTest {
+    private Computer mComputer;
+    @Before
+    public void initialize() {
+        mComputer = new Computer(new ComputerViewModel());
+    }
+    @After
+    public void free() {
+        mComputer = null;
+    }
     @Test
     public void fake() {
-        // fail("No test for view!");
-        Computer computer = new Computer(new ComputerViewModel());
-        computer.solve(0, "", "", "");
-        assertEquals(computer.getOut(), ComputerViewModel.EMPTY_VOLUME_STRING);
+        mComputer.solve(0, "", "", "");
+        assertEquals(mComputer.getOut(),
+                ComputerViewModel.EMPTY_VOLUME_STRING);
     }
 }
