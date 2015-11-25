2ddda201783e46189fb2b25b6be0487557888c09
Voronkov-A <arseny-voronkov@yandex.ru>
2015-11-25 15:45:38 +0300

    fix unused import

--- code/voronkov-arseny/View/src/test/java/ru/unn/agile/VolumesComputer/View/ComputerTest.java
+++ code/voronkov-arseny/View/src/test/java/ru/unn/agile/VolumesComputer/View/ComputerTest.java
@@ -2,7 +2,7 @@ package ru.unn.agile.VolumesComputer.View;
 
 import static org.junit.Assert.*;
 
-import org.junit.Ignore;
+// import org.junit.Ignore;
 import org.junit.Test;
 import ru.unn.agile.VolumesComputer.ViewModel.ComputerViewModel;
 
