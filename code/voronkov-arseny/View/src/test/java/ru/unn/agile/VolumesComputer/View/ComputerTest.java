47ac6870acd56838455c70ab19c4fb66e5c87c95
Voronkov-A <arseny-voronkov@yandex.ru>
2015-11-25 15:49:43 +0300

    add commit

--- code/voronkov-arseny/View/src/test/java/ru/unn/agile/VolumesComputer/View/ComputerTest.java
+++ code/voronkov-arseny/View/src/test/java/ru/unn/agile/VolumesComputer/View/ComputerTest.java
@@ -1,13 +1,9 @@
 package ru.unn.agile.VolumesComputer.View;
 
-// import static org.junit.Assert.*;
-
-// import org.junit.Ignore;
 import org.junit.Test;
 import ru.unn.agile.VolumesComputer.ViewModel.ComputerViewModel;
 
 public class ComputerTest {
-    // @Ignore
     @Test
     public void fake() {
         // fail("No test for view!");
