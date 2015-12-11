0d340071f798885964e0cf4fac4644d97a4e0a28
Gorokhov Dmitriy <gorokhovdmit@gmail.com>
2015-12-11 17:58:08 +0400

    Add logger to view.

--- code/gorokhov-dmitriy/View/src/main/java/ru/unn/agile/MergeSort/View/MergeSortView.java
+++ code/gorokhov-dmitriy/View/src/main/java/ru/unn/agile/MergeSort/View/MergeSortView.java
@@ -1,12 +1,12 @@
 package ru.unn.agile.MergeSort.View;
 
+import ru.unn.agile.MergeSort.Infrastructure.MergeSortFileLogger;
 import ru.unn.agile.MergeSort.ViewModel.MergeSortViewModel;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public final class MergeSortView {
     private final MergeSortViewModel viewModel;
@@ -16,10 +16,14 @@ public final class MergeSortView {
     private JButton sortButton;
     private JTextField statusTextField;
     private JComboBox<MergeSortViewModel.SortingOrder> sortingOrderComboBox;
+    private JList<String> logRecordsList;
+    private static final String LOG_OUTPUT_FILE_NAME = "./MergeSortViewLog.log";
 
     public static void main(final String[] args) {
         JFrame frame = new JFrame("MergeSortView");
-        frame.setContentPane(new MergeSortView(new MergeSortViewModel()).mainPanel);
+
+        MergeSortFileLogger fileLogger = new MergeSortFileLogger(LOG_OUTPUT_FILE_NAME);
+        frame.setContentPane(new MergeSortView(new MergeSortViewModel(fileLogger)).mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
@@ -29,14 +33,15 @@ public final class MergeSortView {
         this.viewModel = viewModel;
 
         loadListOfSortingOrders();
+        sortingOrderComboBox.addActionListener(e -> {
+            mergeSortViewBackBind();
+            mergeSortViewBind();
+        });
 
-        sortButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                mergeSortViewBackBind();
-                viewModel.sort();
-                mergeSortViewBind();
-            }
+        sortButton.addActionListener(e -> {
+            mergeSortViewBackBind();
+            viewModel.sort();
+            mergeSortViewBind();
         });
 
         sourceArrayTextField.getDocument().addDocumentListener(new DocumentListener() {
@@ -73,6 +78,10 @@ public final class MergeSortView {
         statusTextField.setText(viewModel.getSortingArrayStatus());
 
         sortButton.setEnabled(viewModel.isSortButtonEnabled());
+
+        ArrayList<String> recordsList = viewModel.getLogger().getRecordsList();
+        logRecordsList.setListData(recordsList.toArray(new String[recordsList.size()]));
+        logRecordsList.ensureIndexIsVisible(recordsList.size() - 1);
     }
 
     private void loadListOfSortingOrders() {
