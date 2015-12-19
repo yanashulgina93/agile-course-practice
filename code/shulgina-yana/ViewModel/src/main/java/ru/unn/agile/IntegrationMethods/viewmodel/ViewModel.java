619eb96fa9ce93e5c7348a841a559cfb8934d9e5
yanashulgina93 <yana_shulgina93@mail.ru>
2015-12-19 21:49:13 +0300

    reset and attempt to add new changes

--- code/shulgina-yana/ViewModel/src/main/java/ru/unn/agile/IntegrationMethods/viewmodel/ViewModel.java
+++ code/shulgina-yana/ViewModel/src/main/java/ru/unn/agile/IntegrationMethods/viewmodel/ViewModel.java
@@ -70,34 +70,15 @@ public class ViewModel {
         return record;
     }
 
-    private  IFunction createIFunctionObject() {
-        IFunction iFunction;
-        switch (function) {
-            case X:
-                iFunction = new XFunction();
-                break;
-            case COS:
-                iFunction = new CosFunction();
-                break;
-            case EXP:
-                iFunction = new ExpFunction();
-                break;
-            default:
-                throw new IllegalArgumentException("Only x, cos(x) and exp(x) are supported");
-        }
-        return iFunction;
-    }
-
     public void integrate() {
         logger.addRecord(formRecordForLoggerAfterIntegration());
 
         if (!parseLimitsInput()) {
             return;
         }
-        IFunction iFunction = createIFunctionObject();
 
         Integrator integrator = new Integrator(Double.parseDouble(lowerLimit),
-                Double.parseDouble(upperLimit), iFunction);
+                Double.parseDouble(upperLimit), function.getIFunctionObject());
 
         switch (integrationMethod) {
             case LEFT_RECTANGLES:
@@ -210,9 +191,24 @@ public class ViewModel {
     }
 
     public enum Function {
-        X("x"),
-        COS("cos(x)"),
-        EXP("exp(x)");
+        X("x") {
+            @Override
+            public IFunction getIFunctionObject() {
+                return new XFunction();
+            }
+        },
+        COS("cos(x)") {
+            @Override
+            public IFunction getIFunctionObject() {
+                return new CosFunction();
+            }
+        },
+        EXP("exp(x)") {
+            @Override
+            public IFunction getIFunctionObject() {
+                return new ExpFunction();
+            }
+        };
         private final String name;
 
         private Function(final String name) {
@@ -223,6 +219,8 @@ public class ViewModel {
         public String toString() {
             return name;
         }
+
+        public abstract IFunction getIFunctionObject();
     }
 
     public enum IntegrationMethod {
